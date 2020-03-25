46a01d2d41104bb4fbcc93a497d38de8ab2824b5
NILSON AGUIAR ALBUQUERQUE FILHO <nilson.aguiar@unimedfortaleza.com.br>
2020-03-25 18:00:41 -0300

    Adding pagination and profile and session controllers

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,15 +1,34 @@
 const connection = require("../database/connection");
 
+const table_name = "incidents";
+
 module.exports = {
   async index(request, response) {
-    const incidents = await connection.table("incidents").select("*");
+    const { page = 1 } = request.query;
+
+    const [count] = await connection(table_name).count();
+
+    const incidents = await connection(table_name)
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+
+    response.header("X-Total-Count", count["count(*)"]);
     return response.json(incidents);
   },
   async create(request, response) {
     const { title, description, value } = request.body;
     const ong_id = request.headers.authorization;
 
-    const [id] = await connection.table("incidents").insert({
+    const [id] = await connection(table_name).insert({
       title,
       description,
       value,
@@ -17,5 +36,24 @@ module.exports = {
     });
 
     return response.json({ id });
+  },
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection(table_name)
+      .where("id", id)
+      .select("ong_id")
+      .first();
+
+    if (incident.ong_id !== ong_id) {
+      return response.status(401).json({ error: "Operation not permitted" });
+    }
+
+    await connection(table_name)
+      .where("id", id)
+      .delete();
+
+    return response.status(204).send();
   }
 };
---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,21 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+    const ong_id = request.headers.authorization;
+
+    const [count] = await connection(table_name)
+      .where("ong_id", ong_id)
+      .count();
+
+    const incidents = await connection("incidents")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .where("ong_id", ong_id)
+      .select("*");
+
+    response.header("X-Total-Count", count["count(*)"]);
+    return response.json(incidents);
+  }
+};
